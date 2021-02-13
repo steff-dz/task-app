d2785d9e762429014bf23cf27825762c7b7385ca
Steff <stefaniemarie@gmail.com>
2021-02-13 19:32:07 +0100

    putting task app back

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import { useLocalStorage } from './hooks/useLocalStorage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './styles/app.scss';
+import Nav from './comps/Nav';
+import TaskPage from './comps/TaskPage';
+import Calender from './comps/Calender';
+import ExtraPage from './comps/Extrapage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//States for input, tasks, completion status, and display filtering pga status-----------------------------
+	const [ inputTxt, setInputTxt ] = useState('');
+	const [ tasks, setTasks ] = useLocalStorage('tasks', []);
+	const [ status, setStatus ] = useState('all');
+	const [ filteredTasks, setFilteredTasks ] = useState([]);
+
+	//Updates display based off changes to tasks and satus, runs the filtering function-------------------------
+	useEffect(
+		() => {
+			filterHandler();
+		},
+		[ tasks, status ]
+	);
+
+	//Function for filtered display-----------------------------------------------------------------------------
+	const filterHandler = () => {
+		if (status === 'completed') {
+			setFilteredTasks(tasks.filter((task) => task.completed === true));
+		} else if (status === 'uncompleted') {
+			setFilteredTasks(tasks.filter((task) => task.completed === false));
+		} else {
+			setFilteredTasks(tasks);
+		}
+	};
+
+	return (
+		<Router>
+			<div className="App">
+				<Nav />
+				<div className="pageContainer">
+					<Switch>
+						<Route exact path="/">
+							<TaskPage
+								setStatus={setStatus}
+								setTasks={setTasks}
+								tasks={tasks}
+								filteredTasks={filteredTasks}
+								setInputTxt={setInputTxt}
+								inputTxt={inputTxt}
+							/>
+						</Route>
+
+						<Route exact path="/Calender">
+							<Calender />
+						</Route>
+
+						<Route exact path="/extrapg">
+							<ExtraPage />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
